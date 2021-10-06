dc1e69b8b10de8e9aa93b66a085fe10eae778433
xivanc <dano.ivanco@gmail.com>
2021-10-06 18:52:13 +0200

    test

--- src/utils/explorer.ts
+++ src/utils/explorer.ts
@@ -11,6 +11,7 @@ export const getTokenExplorerLink = (
   return `${EXPLORER_MAP[network] || 'https://etherscan.io'}/address/${address}`
 }
 
+
 export const getTxExplorerLink = (network: string, address: string): string => {
   return `${EXPLORER_MAP[network] || 'https://etherscan.io'}/tx/${address}`
 }
